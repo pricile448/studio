283d73ebb6e0cfdd92483e4b9189f2177de93722
Pricile <pricilemenayamondo@gmail.com>
2025-06-23 13:35:32 +0000

    TOUJOURS LE MEME PROBLEME

--- src/lib/dictionaries.ts
+++ src/lib/dictionaries.ts
@@ -1,17 +1,13 @@
 import 'server-only'
+import en from '@/dictionaries/en.json';
+import fr from '@/dictionaries/fr.json';
 
 export type Locale = 'en' | 'fr';
 
-// This is a map of locales to functions that dynamically import the dictionary.
-const dictionaryLoaders = {
-  en: () => import('@/dictionaries/en.json').then((module) => module.default),
-  fr: () => import('@/dictionaries/fr.json').then((module) => module.default),
-};
+const dictionaries = { en, fr };
 
 export const getDictionary = async (locale: Locale) => {
-    // Ensure we have a valid loader, defaulting to 'en'.
-    const loader = dictionaryLoaders[locale] || dictionaryLoaders.en;
-    return loader();
+    return dictionaries[locale] ?? dictionaries.en;
 };
 
 export type Dictionary = Awaited<ReturnType<typeof getDictionary>>;
