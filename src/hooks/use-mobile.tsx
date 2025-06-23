188ba44849ad31e74c7e5789c55c96367f44ddad
Pricile <pricilemenayamondo@gmail.com>
2025-06-23 12:04:39 +0000

    rien du tout une page blanche qui s'affiche

--- src/hooks/use-mobile.tsx
+++ src/hooks/use-mobile.tsx
@@ -3,17 +3,27 @@ import * as React from "react"
 const MOBILE_BREAKPOINT = 768
 
 export function useIsMobile() {
-  const [isMobile, setIsMobile] = React.useState<boolean | undefined>(undefined)
+  const [isMobile, setIsMobile] = React.useState(false)
 
   React.useEffect(() => {
     const mql = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`)
-    const onChange = () => {
-      setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
+
+    // Set the initial state on the client
+    setIsMobile(mql.matches)
+
+    // Create a handler for media query changes
+    const handleChange = (event: MediaQueryListEvent) => {
+      setIsMobile(event.matches)
     }
-    mql.addEventListener("change", onChange)
-    setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
-    return () => mql.removeEventListener("change", onChange)
+
+    // Add the listener for changes
+    mql.addEventListener("change", handleChange)
+
+    // Clean up the listener on unmount
+    return () => {
+      mql.removeEventListener("change", handleChange)
+    }
   }, [])
 
-  return !!isMobile
+  return isMobile
 }
